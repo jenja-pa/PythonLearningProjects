881b419267ce011537219c0b7f3708533619c606
jenja-pa <jenja.pa@gmail.com>
2022-12-21 23:50:31 +0200

    Робота над туторіалом вивчення Selenium 1 - techwithtim

--- selenium/TechWithTim/tutorial1.py
+++ selenium/TechWithTim/tutorial1.py
@@ -1,1 +1,11 @@
-tutorial1.py
+# tutorial1.py
+from selenium import webdriver
+
+
+PATH = r"C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+driver.get("https://techwithtim.net")
+
+print(driver.title)
+driver.close()  # Закрити відкриту процесом вкладку
+# driver.quit()  # Закрити весь браузер
